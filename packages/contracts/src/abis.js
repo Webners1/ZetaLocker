5cd868b3f3fb550c878b2dddff15021973f292d8
webners1 <faizan_babar70@yahoo.com>
2023-09-27 05:27:50 +0500

    add

---
+++ packages/contracts/src/abis.js
@@ -0,0 +1,11 @@
+import erc20Abi from "./abis/erc20.json";
+import ownableAbi from "./abis/ownable.json";
+import tokenLockerAbi from "./abis/tokenLocker.json";
+
+const abis = {
+  erc20: erc20Abi,
+  ownable: ownableAbi,
+  tokenLocker: tokenLockerAbi
+};
+
+export default abis;
